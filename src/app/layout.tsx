2251c4ac60375b20fd8ed31100a6ab9d5907e05c
HessDev <hesspearldev@gmail.com>
2024-11-14 12:08:48 -0300

    added script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,22 @@ export default function RootLayout({
         name="google-site-verification"
         content="LiVSJe5DIzqIn4t8Iak3RF0d1G7DTWQ2h8ShsdCFJPU"
       />
+      {/* Google Tag Manager */}
+      <script>{`(function(w,d,s,l,i){
+      w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NMLW3LQB')`}</script>
+      {/*Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NMLW3LQB"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       <body className={inter.className}>{children}</body>
     </html>
   );
