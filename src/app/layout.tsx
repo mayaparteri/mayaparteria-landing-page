973165a784ff4b2d9fb8324099f8dc52897b3903
HessDev <hesspearldev@gmail.com>
2024-11-17 12:07:26 -0300

    fix gtm tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,12 +41,20 @@ export default function RootLayout({
         content="LiVSJe5DIzqIn4t8Iak3RF0d1G7DTWQ2h8ShsdCFJPU"
       />
       {/* Google Tag Manager */}
-      <script>{`(function(w,d,s,l,i){
-      w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NMLW3LQB')`}</script>
+      <script>
+        {
+          "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NMLW3LQB')"
+        }
+      </script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-3V6LLD4XYG"
+      ></script>
+      <script>
+        {
+          "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-3V6LLD4XYG');"
+        }
+      </script>
       {/*Google Tag Manager (noscript) */}
       <noscript>
         <iframe
